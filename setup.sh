4c8e9a8afc64e93f77b731c3eef60b870aefd9b5
Patrick McCoy <patrick@pjmccoy.com>
2013-06-04 10:12:19 -0600

    Added a check for the OS type

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# check the os type to make sure it is a mac, and exit if it isn't
+if [[ $OSTYPE -ne "darwin11" ]]; then
+    exit 1
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Copy all dotfiles into user's home dir
